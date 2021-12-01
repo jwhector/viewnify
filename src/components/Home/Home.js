8a3aee11bc05d54a41ff06341f3a8ad39ff0f422
ArgenX1 <djc.arizona@gmail.com>
2021-12-01 14:15:11 -0800

    added burger menu

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import './Home.css';
+import './home.css';
 import Discover from '../Discover/Discover';
+import Menu from '../Burger/Burger';
 
 export default function Home(props) {
 
   return (
     <div className="home">
-      <div className="side-bar">
-        <button className="main-links">
-          <p>discover</p>
-        </button>
-        <button className="main-links">
-          <p>watch party</p>
-        </button>
-        <button className="main-links">
-          <p>library</p>
-        </button>
-        <button className="main-links">
-          <p>chatroom</p>
-        </button>
-        <button className="invite-link">
-          <p>invite</p>
-        </button>
-      </div>
+      <Menu />
       <div className="right-container">
         <Discover />
       </div>
