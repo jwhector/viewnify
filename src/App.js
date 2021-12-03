1393ac0d9989b66aea5189bec68b4768d5b850d5
jwhector <jared.hector@gmail.com>
2021-12-02 18:07:53 -0800

    Add background gradient and dynamic box shadows

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
   const [genres, setGenres] = useState('');
   const [providers, setProviders] = useState('');
   const [user, setUser] = useState();
+  const [complementary, setComplementary] = useState('#ffffff');
 
   useEffect(() => {
     if (token) {
@@ -91,29 +92,28 @@ function App() {
   return (
     <>
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <StyledApp class="styleme">
-      <GlobalStyles />
+      <StyledApp className="styleme">
+        <GlobalStyles />
 
-      <NavBar showLogin={false} />
-        <button id="dark-light"onClick={() => themeToggler()}>Change Theme</button>
-      <Router>
-        <Routes>
+        <NavBar showLogin={false} complementary={complementary} setComplementary={setComplementary} />
+        <Router>
+          <Routes>
 
-        <Route path="/" element={<Home page="discover" user={user} token={token} />} />
-        <Route path="/discover" element={<Home page="discover" user={user}  token={token} />} />
-        <Route path="/watchparty" element={<Home page="watchparty" user={user}  token={token} />} />
-        <Route path="/library" element={<Home page="library" user={user}  token={token} />} />
-        <Route path="/chatroom" element={<Home page="chatroom" user={user}  token={token} />} />
-        <Route path="/invite" element={<Home page="invite" user={user} token={token} />} />
-        {/* <Route path="/watchparty" exact component={WatchParty} />
-        <Route path="library" exact component={Library} />
-        <Route path="chatroom" exact component={Chatroom} />
-      <Route path="invite" exact component={Invite} /> */}
-        
-        </Routes>
+          <Route path="/" element={<Home page="discover" user={user} token={token} complementary={complementary} setComplementary={setComplementary} />} />
+          <Route path="/discover" element={<Home page="discover" user={user}  token={token} complementary={complementary} setComplementary={setComplementary} />} />
+          <Route path="/watchparty" element={<Home page="watchparty" user={user}  token={token} setComplementary={setComplementary} />} />
+          <Route path="/library" element={<Home page="library" user={user}  token={token} setComplementary={setComplementary} />} />
+          <Route path="/chatroom" element={<Home page="chatroom" user={user}  token={token} setComplementary={setComplementary} />} />
+          <Route path="/invite" element={<Home page="invite" user={user} token={token} />} setComplementary={setComplementary} />
+          {/* <Route path="/watchparty" exact component={WatchParty} />
+          <Route path="library" exact component={Library} />
+          <Route path="chatroom" exact component={Chatroom} />
+        <Route path="invite" exact component={Invite} /> */}
+          
+          </Routes>
 
-      </Router>
-      </StyledApp>
+          </Router>
+        </StyledApp>
       </ThemeProvider>
     </>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginModal from './Landing/LoginModal';
+import Color from 'color';
 //3005
 
 export default function NavBar(props) {
     const [modalOpen, setModalOpen] = useState(false);
+    const [shadowColor, setShadowColor] = useState('#683fff80')
+
+    // const shadowColor = props.complementary || '#683fff80';
+
+    useEffect(() => {
+        if (props.complementary !== '#683fff80') {
+            let myColor = Color(props.complementary);
+            myColor = myColor.negate();
+            setShadowColor(myColor);
+        } else {
+            setShadowColor(props.complementary);
+        }
+        // console.log(shadowColor);
+    }, [props.complementary]);
 
     const setLogin = (e) => {
         setModalOpen(true);
@@ -14,7 +29,7 @@ export default function NavBar(props) {
     }
 
     return (
-        <div id="nav">
+        <div id="nav" style={{ boxShadow: `0px 5px 6px ${shadowColor}` }}>
             <h1 className="text-logo"><span className="vertical-ctr">view<span className="bold">nify</span></span></h1>
             <h1 className="text-logo shadowed"><span className="vertical-ctr">view<span className="bold">nify</span></span></h1>
             {props.showLogin ?
