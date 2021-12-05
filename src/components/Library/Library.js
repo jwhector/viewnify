3860eb6b752b3498a0a3706765afae074cbce3ee
jwhector <jared.hector@gmail.com>
2021-12-05 05:22:09 -0800

    Add library grid

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -1,4 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import MiniCard from '../Card/MiniCard';
+
+function MiniCards(props) {
+    const media = props.media;
+    const miniCards = media.map((medium) => {
+        const poster = `https://image.tmdb.org/t/p/original${medium.poster_path}`;
+        const backdrop = `https://image.tmdb.org/t/p/original${medium.backdrop_path}`;
+        const title = medium.title;
+        const runtime = medium.runtime;
+        const overview = medium.overview;
+        const rating = medium.vote_average;
+        // const poster = 
+        return <li key={medium.id}><MiniCard poster={poster} backdrop={backdrop} title={title} runtime={runtime} overview={overview} rating={rating} /></li>
+    });
+    return <ul className="library-list">{miniCards}</ul>
+}
 
 /* PROPS
     
@@ -15,15 +31,21 @@ export default function Library(props) {
       }).then(res => {
         res.json().then(data => {
             console.log(data)
+            setMedia(data);
         });
       });
     }, []);
 
+    // const MiniCards = (props) => {
+    //     const 
+    // }
+
     return (
-        <div id="library">
+        <div className="library">
             <h2>Library</h2>
             <hr/>
             <div className="library-grid">
+                <MiniCards media={media} />
             </div>
         </div>
     );
