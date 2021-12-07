ab33d9f6b8df89889d148c32198ed025e92fecbd
jwhector <jared.hector@gmail.com>
2021-12-06 17:52:12 -0800

    Fix discover card styling and remove mini-card whitespace

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -60,24 +60,28 @@ export default function Card(props) {
     
   return (
     <div className={`media-main ${props.aOrB ? props.aOrB : ''} ${props.isFront ? 'front' : 'back'}`} style={{ boxShadow: `4px 4px 8px ${props.complementary}` }} ref={elem}>
-        <div className="content-gradient-overlay" />
-        <div className="content-img">
-            <img className="cur-content-img" src={props.images[props.idx]} />
-        </div>
-        <div className="content-description hidden">
-            <img className="content-background" src={props.media[props.idx]?.backdrop} />
-            <div className="content-title">
-                <h2 className="info-title">{props.media[props.idx]?.title}</h2>
+        <div className='card-content'>
+            <div className="content-img">
+                <div className="content-gradient-overlay" />
+                <img className="cur-content-img" src={props.images[props.idx]} />
             </div>
-            <div className="media-a-info" style={{background: `linear-gradient(180deg, ${props.bgColor} 0%, transparent 3%)`,}}>
-                <div className="release-rating card-info">
-                <p className="release-date card-info">Release Date: {props.media[props.idx]?.release_date}</p>
-                <p className="rating card-info">Rating: {props.media[props.idx]?.rating}</p>
+            <div className="content-description hidden">
+                <div className="background-container">
+                    <img className="content-background" src={props.media[props.idx]?.backdrop} />
+                    <div className="content-title">
+                        <h2 className="info-title">{props.media[props.idx]?.title}</h2>
+                    </div>
                 </div>
-                <p className="card-info">{props.media[props.idx]?.overview}</p>
+                <div className="media-a-info" style={{background: `linear-gradient(180deg, ${props.bgColor} 0%, transparent 3%)`,}}>
+                    <div className="release-rating card-info">
+                    <p className="release-date card-info">Release Date: {props.media[props.idx]?.release_date}</p>
+                    <p className="rating card-info">Rating: {props.media[props.idx]?.rating}</p>
+                    </div>
+                    <p className="card-info">{props.media[props.idx]?.overview}</p>
+                </div>
             </div>
         </div>
-        <div className="bottom-border" />
+        {props.isDiscover ? <div className="bottom-border" /> : <></>}
     </div>
   );
 }
