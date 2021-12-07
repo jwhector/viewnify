eb2183c52cd27386b9252b9fc615d1972e2be23a
jwhector <jared.hector@gmail.com>
2021-12-06 22:41:42 -0800

    Change genremap to str

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -8,25 +8,25 @@ import Swipe from '../Swipe/Swipe';
 import { UserContext } from '../../App';
 
 const genreMap = {
-  "Action": 28,
-  "Adventure": 12,
-  "Animation": 16,
-  "Comedy": 35,
-  "Crime": 80,
-  "Drama": 18,
-  "Family": 10751,
-  "Fantasy": 14,
-  "History": 36,
-  "Horror": 27,
-  "Music": 10402,
-  "Mystery": 9648,
-  "Romance": 10749,
-  "Science Fiction": 878,
-  "TV Movie": 10770,
-  "Thriller": 53,
-  "War": 10752,
-  "Western": 37,
-  }
+  "28": "Action",
+  "12": "Adventure",
+  "16": "Animation",
+  "35": "Comedy",
+  "80": "Crime",
+  "18": "Drama",
+  "10751": "Family",
+  "14": "Fantasy",
+  "36": "History",
+  "27": "Horror",
+  "10402": "Music",
+  "9648": "Mystery",
+  "10749": "Romance",
+  "878": "Science Fiction",
+  "10770": "TV Movie",
+  "53": "Thriller",
+  "10752": "War",
+  "37": "Western",
+}
 
 export default function Discover(props) {
   const [images, setImages] = useState([]);
@@ -108,7 +108,7 @@ export default function Discover(props) {
   }
 
   const getGenres = (genre_ids) => {
-    return genre_ids.map(genre_id => genreMap[genre_id]);
+    return genre_ids.map(genre_id => genreMap[`${genre_id}`]);
   }
 
   const getEntries = async () => {
--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -28,25 +28,25 @@ function MiniCards(props) {
 
 
 const genreMap = {
-    "Action": 28,
-    "Adventure": 12,
-    "Animation": 16,
-    "Comedy": 35,
-    "Crime": 80,
-    "Drama": 18,
-    "Family": 10751,
-    "Fantasy": 14,
-    "History": 36,
-    "Horror": 27,
-    "Music": 10402,
-    "Mystery": 9648,
-    "Romance": 10749,
-    "Science Fiction": 878,
-    "TV Movie": 10770,
-    "Thriller": 53,
-    "War": 10752,
-    "Western": 37,
-}
+    "28": "Action",
+    "12": "Adventure",
+    "16": "Animation",
+    "35": "Comedy",
+    "80": "Crime",
+    "18": "Drama",
+    "10751": "Family",
+    "14": "Fantasy",
+    "36": "History",
+    "27": "Horror",
+    "10402": "Music",
+    "9648": "Mystery",
+    "10749": "Romance",
+    "878": "Science Fiction",
+    "10770": "TV Movie",
+    "53": "Thriller",
+    "10752": "War",
+    "37": "Western",
+  }
 
 
 ReactModal.setAppElement('#root');
@@ -115,7 +115,7 @@ export default function Library(props) {
       });
 
     const getGenres = (genre_ids) => {
-        return genre_ids.map(genre_id => genreMap[genre_id]);
+        return genre_ids.map(genre_id => genreMap[`${genre_id}`]);
     }
 
     const openModal = (e) => {
