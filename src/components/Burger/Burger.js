821dacc807d587716a986867f94d0ecfc47f40e1
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-12-07 04:28:36 -0800

    organizing card-2 on discover swipes

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,8 +4,6 @@ import { render } from '@testing-library/react';
 import {NavLink} from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
-import { cgSun } from "react-icons/cg";
-import { HiMoon } from "react-icons/hi"
 import useToken from "../../useToken";
 import Preferences from '../Preferences/PreferencesPage'
 import ReactModal from 'react-modal';
@@ -171,12 +169,11 @@ export default function Burger(props) {
   return (
 
     <Menu isOpen={menuOpen}>
-        <NavLink exact activeClassName="main-links" to="/discover" onClick={() => this.closeMenu()} id='discover-btn'>Discover</NavLink>
-        <NavLink exact activeClassName="main-links" to="/watchparty" onClick={() => this.closeMenu()} id='watch-party'>Watch Party</NavLink>
-        <NavLink exact activeClassName="main-links" to="/library" onClick={() => this.closeMenu()} id="library">Library</NavLink>
-        <NavLink exact activeClassName="main-links" to="/invite" id="invite">Invite</NavLink>
-        <NavLink exact activeClassName="main-links" to='' onClick={openModal}  id="preferences">Preferences</NavLink>
-        <NavLink exact activeClassName="main-links" to="/" onClick={removeToken} id="logout">Log Out</NavLink>
+        <NavLink exact activeClassName="main-links" to="/discover" onClick={() => this.closeMenu()} id='discover-btn'>discover</NavLink>
+        <NavLink exact activeClassName="main-links" to="/watchparty" onClick={() => this.closeMenu()} id='watch-party'>watch party</NavLink>
+        <NavLink exact activeClassName="main-links" to="/library" onClick={() => this.closeMenu()} id="library">library</NavLink>
+        <NavLink exact activeClassName="main-links" to='' onClick={openModal}  id="preferences-btn">preferences</NavLink>
+        <NavLink exact activeClassName="main-links" to="/" onClick={removeToken} id="logout">log out</NavLink>
         {/* <button id="dark-light"onClick={() => props.themeToggler()}>Change Theme</button> */}
         
         <ReactModal isOpen={modalOpen}
