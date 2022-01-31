03c77cb2421f3081453787efd512dfa356d9f32c
kelliekumasaka <kelliek3@gmail.com>
2022-01-30 17:32:22 -0800

    css additions

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Home.css';
+import './home.css';
 import Discover from '../Discover/Discover';
 import Menu from '../Burger/Burger';
 import { useState } from 'react';
